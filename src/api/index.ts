556fc6fd1bfc07c5fdf4b1e17b0257db40e22bbe
Ko-GyeongTae <kokt0203@naver.com>
2021-03-16 12:00:20 +0900

    .js -> .ts

---
+++ src/api/index.ts
@@ -0,0 +1,11 @@
+import Router from "koa-router";
+
+const api = new Router();
+
+api.get("/", );
+api.get("/home", );
+api.get("/profile", );
+api.get("/activity", );
+api.get("/post/:id", );
+
+export default api;
