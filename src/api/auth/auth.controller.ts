2da5065625149262bea8569a283a67a2d56f1343
Ko-GyeongTae <kokt0203@naver.com>
2021-04-01 19:51:54 +0900

    Edit: Signup Logic

--- src/api/auth/auth.controller.ts
+++ src/api/auth/auth.controller.ts
@@ -3,28 +3,46 @@ import { getConnection } from "typeorm";
 import { User } from "../../entity/User";
 dotenv.config();
 
+export const confirmName = (async(name, email) => {
+    console.log(name, email);
+    let result = await getConnection().getRepository(User).createQueryBuilder()
+    .where("user.name = :name", {name: name})
+    .getOne();
+    if(result === undefined){
+        result = await getConnection().getRepository(User).createQueryBuilder()
+        .where("user.email = :email", {email: email})
+        .getOne();
+    }
+    console.log(result);
+    return result;
+});
+
 export const signup = (async(ctx) => {
     const req = ctx.request.body;
     let body, status; 
     console.log(req); 
     const connection = await getConnection();
+    const confirmObj = await confirmName(req.name, req.email);
     const TimeStamp = Date.now();
     const date = new Date(TimeStamp);
     const user = new User();
     if(req.name === "" || req.email === ""){
         status = 400;
         body = "Fill information";
-    } else {
+    } else if(req.name === confirmObj.name || req.email === confirmObj.email) {
+        status = 400;
+        body = "You can't account";
+    }else {
         user.name = req.name;
         user.email = req.email;
         user.password = req.password;
         user.createdAt = date;
         status = 200;
         body = user;
+        //await connection.manager.save(user);
     }
     ctx.body = body;
     ctx.status = status;
-    //await connection.manager.save(user);
 });
 
 export const login = (async(ctx) => {
